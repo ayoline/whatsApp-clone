5ed019ffc7fca2cdc77cf8aa183eba50a0592cd9
ayoline <ayo@outlook.com.br>
2021-08-29 20:52:01 -0300

    Configura cor e alinhamento a tela de mensagens

--- lib/Mensagens.dart
+++ lib/Mensagens.dart
@@ -12,13 +12,126 @@ class Mensagens extends StatefulWidget {
 }
 
 class _MensagensState extends State<Mensagens> {
+  List<String> listaMensagens = [
+    "teste 1,teste 1,teste 1,teste 1,teste 1,teste 1,teste 1,teste 1,teste 1,teste 1,teste 1,teste 1,",
+    "teste 2,teste 2,teste 2,teste 2,teste 2,teste 2,teste 2,teste 2,teste 2,teste 2,teste 2,teste 2,",
+    "teste 3",
+    "teste 4",
+    "teste 5",
+    "teste 6",
+    "teste 7",
+    "teste 8",
+    "teste 9",
+    "teste 10",
+  ];
+
+  TextEditingController _controllerMensagem = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    var caixaMensagem = Container(
+      padding: EdgeInsets.all(8),
+      child: Row(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(right: 8),
+              child: TextField(
+                controller: _controllerMensagem,
+                autofocus: true,
+                keyboardType: TextInputType.text,
+                style: TextStyle(fontSize: 20),
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.fromLTRB(32, 8, 32, 8),
+                  hintText: "Digite uma mensagem...",
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(32),
+                  ),
+                  prefixIcon: IconButton(
+                    onPressed: _enviarFoto,
+                    icon: Icon(Icons.camera_alt),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          FloatingActionButton(
+            backgroundColor: Color(0xff075E54),
+            child: Icon(
+              Icons.send,
+              color: Colors.white,
+            ),
+            mini: true,
+            onPressed: _enviarMensagem,
+          ),
+        ],
+      ),
+    );
+
+    var listView = Expanded(
+      child: ListView.builder(
+          itemCount: listaMensagens.length,
+          itemBuilder: (context, indice) {
+            double larguraContainer = MediaQuery.of(context).size.width * 0.8;
+
+            // Define cores e alinhamentos
+            Alignment alinhamento = Alignment.centerRight;
+            Color cor = Color(0xffd2ffa5);
+            if (indice % 2 == 0) {
+              alinhamento = Alignment.centerLeft;
+              cor = Colors.white;
+            }
+
+            return Align(
+              alignment: alinhamento,
+              child: Padding(
+                padding: EdgeInsets.all(6),
+                child: Container(
+                  width: larguraContainer,
+                  padding: EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: cor,
+                    borderRadius: BorderRadius.all(Radius.circular(8)),
+                  ),
+                  child: Text(
+                    listaMensagens[indice],
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+              ),
+            );
+          }),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.contato.nome),
       ),
-      body: Container(),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/bg.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: SafeArea(
+            child: Container(
+          padding: EdgeInsets.all(8),
+          child: Column(
+            children: [
+              listView,
+              caixaMensagem,
+            ],
+          ),
+        )),
+      ),
     );
   }
+
+  _enviarMensagem() {}
+
+  _enviarFoto() {}
 }
